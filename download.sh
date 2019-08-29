38efaf05aa50ddd729aea7ddbfe6f812c9640dfb
Petr Smirnov <psmirnov2000@gmail.com>
2019-08-29 14:54:50 -0400

    first add download script

---
+++ download.sh
@@ -0,0 +1,5 @@
+cd /pfs/out
+wget https://media.nature.com/original/nature-assets/nature/journal/v540/n7631/extref/nature20171-s1.xls
+wget https://media.nature.com/original/nature-assets/nature/journal/v540/n7631/extref/nature20171-s2.xlsx
+
+
